a21f6299009d015b13339534b7846ae4988cb19c
dylandrobi <124712580+dylandrobi@users.noreply.github.com>
2023-03-06 18:30:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 //=====[Libraries]=============================================================
 
-#include "smart_home_system.h"
+#include "alarm_clock_system.h"
 
 //=====[Main function, the program entry point after power on or reset]========
 
 int main()
 {
-    smartHomeSystemInit();
+    alarmClockSystemInit();
     while (true) {
-        smartHomeSystemUpdate();
+        alarmClockSystemUpdate();
     }
 }
